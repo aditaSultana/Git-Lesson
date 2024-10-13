88958bcfeaf749f8513a71fd80c2bdaab8b6e7a6
aditaSultana <adita.zedan@gmail.com>
2024-10-13 09:44:13 -0400

    Added 2 files

---
+++ calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+
+    int A = 20;
+    int B = 50;
+
+    System.out.println("Sum"+ "="+ A+B);
+    
+    System.out.println("Substraction"+ "="+ A-B);
+
+    System.out.println("Multiplication"+ "="+ A*B);
+}
